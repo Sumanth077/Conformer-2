eb2598979f4af42992d652533314b4f17a41df82
Sumanth <66694715+Sumanth077@users.noreply.github.com>
2023-07-21 14:38:24 +0530

    Update first_page.py

--- pages/first_page.py
+++ pages/first_page.py
@@ -1,9 +1,11 @@
 import streamlit as st
+import assemblyai as aai
+
 from clarifai.auth.helper import ClarifaiAuthHelper
 from clarifai.client import create_stub
+from clarifai_utils.modules.css import ClarifaiStreamlitCSS
 from clarifai.listing.lister import ClarifaiResourceLister
 from clarifai.modules.css import ClarifaiStreamlitCSS
-from google.protobuf import json_format, timestamp_pb2
 
 st.set_page_config(layout="wide")
 ClarifaiStreamlitCSS.insert_default_css(st)
@@ -13,37 +15,28 @@ auth = ClarifaiAuthHelper.from_streamlit(st)
 stub = create_stub(auth)
 userDataObject = auth.get_user_app_id_proto()
 lister = ClarifaiResourceLister(stub, auth.user_id, auth.app_id, page_size=16)
-st.title("Simple example to list inputs")
 
-with st.form(key="data-inputs"):
-  mtotal = st.number_input(
-      "Select number of inputs to view in a table:", min_value=10, max_value=100)
-  submitted = st.form_submit_button('Submit')
+st.title("🎙️ Audio Transcription using the latest Conformer-2 Model from Assemblyai")
 
-if submitted:
-  if mtotal is None or mtotal == 0:
-    st.warning("Number of inputs must be provided.")
-    st.stop()
-  else:
-    st.write("Number of inputs in table will be: {}".format(mtotal))
+st.sidebar.markdown("Made with UI Modules, a streamlit integration from [Clarifai](https://clarifai.com/) that helps you create and deploy beautiful AI web apps.")
+st.sidebar.image("https://clarifai.com/favicon.svg", width=100)
 
-  # Stream inputs from the app
-  all_inputs = []
-  for inp in lister.inputs_generator():
-    all_inputs.append(inp)
 
-    if len(all_inputs) >= mtotal:
-      break
+# set the API key
+with st.sidebar:
+    api_key = st.text_input("AssemblyAI API Key", type="password")
+    aai.settings.api_key = f"{api_key}"
 
-  # Get some common stuff out of the inputs.
-  data = []
-  for inp in all_inputs:
-    data.append({
-        "id": inp.id,
-        "status": inp.status.description,
-        "created_at": timestamp_pb2.Timestamp.ToDatetime(inp.created_at),
-        "modified_at": timestamp_pb2.Timestamp.ToDatetime(inp.modified_at),
-        "metadata": json_format.MessageToDict(inp.data.metadata),
-    })
+def transcribe(audio):
+    transcriber = aai.Transcriber()
+    transcript = transcriber.transcribe("https://example.org/audio.mp3", config=aai.TranscriptionConfig(summarization=True))
+    st.info(transcript)
 
-  st.dataframe(data)
+
+with st.form("my_form"):
+    audio = st.text_area("Enter text:", "Enter the Audio URL that you want to transcribe?")
+    submitted = st.form_submit_button("Submit")
+    if not api_key:
+        st.info("Please add your AssemblyAI API key to continue.")
+    elif submitted:
+        transcribe(audio)
